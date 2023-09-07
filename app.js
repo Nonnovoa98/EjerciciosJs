8df7316734a17403aa484901aacb2ddaabe96ed3
Nonnovoa98 <142616888+Nonnovoa98@users.noreply.github.com>
2023-09-06 21:12:32 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+//CONTADOR ASCENDENTE: imprime los numeros del 1 a 10.
+
+// for(let i=1; i<=10; i++){
+//     console.log(i)
+// }
+
+
+
+//CONTADOR DESCENDENTE: Imprime los números del 10 al 1.
+
+// for(let i=10; i>=1; i--){
+//     console.log(i)
+// }
+
+
+
+//SUMA DE NUMEROS: Suma de los numeros del 1 a 10.
+
+// let sum=0
+// for(let i=1; i<=10; i++){
+//     sum=sum+i
+// }
+// console.log(sum)
+
+
+//TABLA DE MULTIPLICAR
+
+//  let   num = parseInt(prompt("ingrese un numero:")) 
+
+//  for (let i=0; i<=10; i++){
+//     console.log(num*i)
+//  }
+
+
+
+//CONTADOR DE PARES: Imprime los números pares del 2 al 20.
+
+// for(let i=2; i<=20; i=i+2){
+//     console.log(i)
+// }
+
+
+
+//CONTADOR DE IMPARES: Imprime los números impares del 1 al 19.
+
+// for(let i=1; i<20; i=i+2){
+//     console.log(i)
+// }
+
+//CONTADOR DE DIGITOS:
+
+// let num=(parseInt(prompt("Ingrese un numero")))
+// let aux=0
+// let c=0
+
+// while(num>=1){
+//     aux=parseInt(num/10)
+//     num=aux
+//     c++
+// }
+
+// console.log("Elnumero ingresado tiene ", c, "digitos.")
